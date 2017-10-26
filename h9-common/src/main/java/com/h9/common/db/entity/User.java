58087a9bbd07f311cb76f95cc3f44598faa4a99e
shadow.liu <shadow.liu@hey900.com>
2017-10-26 11:28:36 +0800

    提交代码

---
+++ h9-common/src/main/java/com/h9/common/db/entity/User.java
@@ -0,0 +1,36 @@
+package com.h9.common.db.entity;
+
+import com.h9.common.base.BaseEntity;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User:刘敏华 shadow.liu@hey900.com
+ * Date: 2017/10/26
+ * Time: 11:16
+ */
+
+@Entity
+@Table(name = "user")
+public class User extends BaseEntity {
+
+
+    @Id
+    @SequenceGenerator(name = "h9-apiSeq", sequenceName = "h9-api_SEQ", allocationSize = 1, initialValue = 1)
+    @GeneratedValue(strategy = IDENTITY, generator = "h9-apiSeq")
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+}
