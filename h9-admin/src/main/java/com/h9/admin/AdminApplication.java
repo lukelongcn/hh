6efa40f289a1748aea7d42ef220480afa0d370bc
shadow.liu <shadow.liu@hey900.com>
2017-10-26 11:35:05 +0800

    提交代码

--- h9-admin/src/main/java/com/h9/admin/AdminApplication.java
+++ h9-admin/src/main/java/com/h9/admin/AdminApplication.java
@@ -2,8 +2,10 @@ package com.h9.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.h9.common","com.h9.admin"})
 public class AdminApplication {
 
 	public static void main(String[] args) {
