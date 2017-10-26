da8618fa9da17cccc5dfa43918142b886c615bd0
longdenghui@hey900.com <longdenghui@hey900.com>
2017-10-26 18:23:54 +0800

    调整启动banner

--- h9-admin/src/main/java/com/h9/admin/AdminApplication.java
+++ h9-admin/src/main/java/com/h9/admin/AdminApplication.java
@@ -1,16 +1,40 @@
 package com.h9.admin;
 
+import org.jboss.logging.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.h9.common","com.h9.admin"})
+@ComponentScan(basePackages = {"com.h9.common", "com.h9.admin"})
 public class AdminApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdminApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+
+        Logger logger = Logger.getLogger(AdminApplication.class);
+        logger.info("\n" +
+                "                                        _ooOoo_\n" +
+                "                                       o8888888o\n" +
+                "                                       88\\\" . \\\"88\n" +
+                "                                        (| -_- |)\n" +
+                "                                       O\\\\  =  /O\n" +
+                "                                    ____/`---'\\\\____\n" +
+                "                                  .'  \\\\\\\\|     |//  `.\n" +
+                "                                 /  \\\\\\\\|||  :  |||//  \\\\\n" +
+                "                                /    _||||| -:- |||||-  \\\\\n" +
+                "                                |  | \\\\\\\\\\\\  -  /// |   |\n" +
+                "                                | \\\\_|  ''\\\\---/''  |   |\n" +
+                "                                \\\\  .-\\\\__  `-`  ___/-. /\n" +
+                "                              ___`. .'  /--.--\\\\  `. . __\n" +
+                "                           .\\\"\\\" '<  `.___\\\\_<|>_/___.'  >'\\\"\\\".\n" +
+                "                          | | :  `- \\\\`.;`\\\\ _ /`;.`/ - ` : | |\n" +
+                "                          \\\\  \\\\ `-.   \\\\_ __\\\\ /__ _/   .-` /  /\n" +
+                "                     ======`-.____`-.___\\\\_____/___.-`____.-'======\n" +
+                "                                        `=---='\n" +
+                "                     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n" +
+                "                                佛祖保佑  永不冗机  永无BUG");
+    }
 
 
 }
--- h9-api/src/main/java/com/h9/api/ApiApplication.java
+++ h9-api/src/main/java/com/h9/api/ApiApplication.java
@@ -1,5 +1,6 @@
 package com.h9.api;
 
+import org.jboss.logging.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,6 +13,28 @@ public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+		Logger logger = Logger.getLogger(ApiApplication.class);
+		logger.info("\n" +
+				"                                        _ooOoo_\n" +
+				"                                       o8888888o\n" +
+				"                                       88\\\" . \\\"88\n" +
+				"                                        (| -_- |)\n" +
+				"                                       O\\\\  =  /O\n" +
+				"                                    ____/`---'\\\\____\n" +
+				"                                  .'  \\\\\\\\|     |//  `.\n" +
+				"                                 /  \\\\\\\\|||  :  |||//  \\\\\n" +
+				"                                /    _||||| -:- |||||-  \\\\\n" +
+				"                                |  | \\\\\\\\\\\\  -  /// |   |\n" +
+				"                                | \\\\_|  ''\\\\---/''  |   |\n" +
+				"                                \\\\  .-\\\\__  `-`  ___/-. /\n" +
+				"                              ___`. .'  /--.--\\\\  `. . __\n" +
+				"                           .\\\"\\\" '<  `.___\\\\_<|>_/___.'  >'\\\"\\\".\n" +
+				"                          | | :  `- \\\\`.;`\\\\ _ /`;.`/ - ` : | |\n" +
+				"                          \\\\  \\\\ `-.   \\\\_ __\\\\ /__ _/   .-` /  /\n" +
+				"                     ======`-.____`-.___\\\\_____/___.-`____.-'======\n" +
+				"                                        `=---='\n" +
+				"                     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n" +
+				"                                佛祖保佑  永不冗机  永无BUG");
 	}
 
 
