3776cd4512c307b037746f4ab2d023e65b20204d
shadow.liu <shadow.liu@hey900.com>
2017-10-26 15:00:12 +0800

    项目配置

--- h9-api/src/main/java/com/h9/api/ApiApplication.java
+++ h9-api/src/main/java/com/h9/api/ApiApplication.java
@@ -2,8 +2,10 @@ package com.h9.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.h9.common","com.h9.api"})
 public class ApiApplication {
 
 	public static void main(String[] args) {
