9c5d8577e982712a767171644104085c57e62447
shadow.liu <shadow.liu@hey900.com>
2017-10-26 10:46:50 +0800

    初始化导入

---
+++ h9-admin/src/main/java/com/h9/admin/AdminApplication.java
@@ -0,0 +1,14 @@
+package com.h9.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminApplication.class, args);
+	}
+
+
+}
