753fe507fad02daa1a2b2ed3250de077c3c7a3ed
qiuhonghui <qiuhonghui@hey900.com>
2017-10-27 20:03:44 +0800

    增加请求和相应日子打印

---
+++ h9-admin/src/main/java/com/h9/admin/interceptor/InterceptorConfig.java
@@ -0,0 +1,17 @@
+package com.h9.admin.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by itservice on 2017/10/26.
+ */
+@Configuration
+public class InterceptorConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(new HttpModelInterceptor()).addPathPatterns("/**");
+    }
+}
