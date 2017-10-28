004ea2621e86714c6aef6bfceac172547f621417
shadow.liu <shadow.liu@hey900.com>
2017-10-28 09:31:50 +0800

    redis的key

---
+++ h9-common/src/main/java/com/h9/common/db/bean/RedisKey.java
@@ -0,0 +1,13 @@
+package com.h9.common.db.bean;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:redis的键值
+ * RedisKey:刘敏华 shadow.liu@hey900.com
+ * Date: 2017/10/28
+ * Time: 9:31
+ */
+public class RedisKey {
+
+
+}
