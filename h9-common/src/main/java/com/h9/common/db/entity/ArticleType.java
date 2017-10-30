e2c0f9e1763e4dc9a3cdd8285947351d48ccd847
shadow.liu <shadow.liu@hey900.com>
2017-10-30 15:39:11 +0800

    统一路径

---
+++ h9-common/src/main/java/com/h9/common/db/entity/ArticleType.java
@@ -0,0 +1,70 @@
+package com.h9.common.db.entity;
+
+import com.h9.common.base.BaseEntity;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:banner类型
+ * User:刘敏华 shadow.liu@hey900.com
+ * Date: 2017/10/27
+ * Time: 18:13
+ */
+@Entity
+@Table(name = "article_type")
+public class ArticleType extends BaseEntity {
+
+
+    @Id
+    @SequenceGenerator(name = "h9-apiSeq", sequenceName = "h9-api_SEQ", allocationSize = 1, initialValue = 1)
+    @GeneratedValue(strategy = IDENTITY, generator = "h9-apiSeq")
+    private Long id;
+
+    @Column(name = "name", nullable = false, columnDefinition = "varchar(64) default '' COMMENT 'banner类型名称'")
+    private String name;
+
+    @Column(name = "code", nullable = false, columnDefinition = "varchar(64) default '' COMMENT 'banner类型标识'")
+    private String code;
+
+
+    @Column(name = "enable",nullable = false,columnDefinition = "tinyint default 1 COMMENT '是否启用 1启用 0 禁用'")
+    private Integer enable;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Integer getEnable() {
+        return enable;
+    }
+
+    public void setEnable(Integer enable) {
+        this.enable = enable;
+    }
+
+
+}
