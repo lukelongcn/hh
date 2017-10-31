07b45075f7f2b977c05ccc08979514a62a0073cc
shadow.liu <shadow.liu@hey900.com>
2017-10-31 11:23:30 +0800

    增加静态页面配置

---
+++ h9-api/src/main/java/com/h9/api/controller/CommonController.java
@@ -0,0 +1,17 @@
+package com.h9.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:TODO
+ * CommonController:刘敏华 shadow.liu@hey900.com
+ * Date: 2017/10/31
+ * Time: 11:22
+ */
+@RestController
+@RequestMapping("common")
+public class CommonController {
+}
