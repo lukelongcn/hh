b53dc327131aa5f07363cd980ecfc325cfedb132
qiuhonghui <qiuhonghui@hey900.com>
2017-10-31 19:14:29 +0800

    增加HttpUtil

--- h9-common/src/main/java/com/h9/common/utils/HttpUtil.java
+++ h9-common/src/main/java/com/h9/common/utils/HttpUtil.java
@@ -1,8 +1,32 @@
 package com.h9.common.utils;
 
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 /**
  * @author: George
  * @date: 2017/10/31 17:51
  */
 public class HttpUtil {
+
+    public static HttpServletRequest getHttpServletRequest(){
+        return getServletRequestAttributes().getRequest();
+    }
+
+    public static HttpServletResponse getHttpServletResponse(){
+        return getServletRequestAttributes().getResponse();
+    }
+
+    public static HttpSession getHttpSession(){
+        return getHttpServletRequest().getSession();
+    }
+
+    public static ServletRequestAttributes getServletRequestAttributes(){
+        return (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+    }
+
 }
