4334670b5b103135fddf1fbb2f8f164bda74ca31
shadow.liu <shadow.liu@hey900.com>
2017-11-01 16:00:59 +0800

    公共模块

--- h9-api/src/main/java/com/h9/api/controller/CommonController.java
+++ h9-api/src/main/java/com/h9/api/controller/CommonController.java
@@ -2,6 +2,8 @@ package com.h9.api.controller;
 
 import com.h9.api.interceptor.Secured;
 import com.h9.api.provider.WeChatProvider;
+import com.h9.common.db.repo.AgreementRepository;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
