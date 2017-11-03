4c45d5df3625b11e4b2e9a4a393cac5a182d2393
qiuhonghui <qiuhonghui@hey900.com>
2017-11-03 14:22:05 +0800

    初始化bannerType和articleType

---
+++ h9-common/src/main/java/com/h9/common/db/repo/ArticleTypeRepository.java
@@ -0,0 +1,8 @@
+package com.h9.common.db.repo;
+
+/**
+ * @author: George
+ * @date: 2017/11/3 14:13
+ */
+public interface ArticleTypeRepository {
+}
---
+++ h9-common/src/main/java/com/h9/common/db/repo/GlobalPropertyRepository.java
@@ -0,0 +1,8 @@
+package com.h9.common.db.repo;
+
+/**
+ * @author: George
+ * @date: 2017/11/3 10:57
+ */
+public interface GlobalPropertyRepository {
+}
