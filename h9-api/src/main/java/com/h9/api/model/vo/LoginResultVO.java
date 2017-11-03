b6f011d3e6bb81b421e4d83163a0806cca81cca2
Chaos <liyuan@hey900.com>
2017-11-03 10:54:36 +0800

    修改VO转换

--- h9-api/src/main/java/com/h9/api/model/vo/LoginResultVO.java
+++ h9-api/src/main/java/com/h9/api/model/vo/LoginResultVO.java
@@ -1,9 +1,7 @@
 package com.h9.api.model.vo;
 
 import com.h9.common.db.entity.User;
-import com.h9.common.db.entity.UserAccount;
-
-import java.math.RoundingMode;
+import org.springframework.beans.BeanUtils;
 
 /**
  * Created by itservice on 2017/10/28.
@@ -14,6 +12,15 @@ public class LoginResultVO {
     private String imgUrl;
     private String nickName;
     private String openId;
+
+    public LoginResultVO() {
+    }
+
+    public LoginResultVO(User user, String token) {
+        BeanUtils.copyProperties(user,this);
+        this.token = token;
+        imgUrl =  user.getAvatar();
+    }
 
     public static LoginResultVO convert(User user, String token){
         LoginResultVO vo = new LoginResultVO();
