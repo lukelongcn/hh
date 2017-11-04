8f35ecf9f0e2b28e4463c353fbcf09bd4531e0f7
Chaos <liyuan@hey900.com>
2017-11-04 15:32:58 +0800

    银行类别

---
+++ h9-common/src/main/java/com/h9/common/db/repo/BankTypeRepository.java
@@ -0,0 +1,18 @@
+package com.h9.common.db.repo;
+
+import com.h9.common.base.BaseRepository;
+import com.h9.common.db.entity.BankType;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by 李圆
+ * on 2017/11/4
+ */
+@Repository
+public interface BankTypeRepository  extends BaseRepository<BankType> {
+    @Query("select bankName from BankType")
+    List<String> findBankName();
+}
