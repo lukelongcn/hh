68b6f86d7b5732303f1620988a5cc33310ddac00
shadow.liu <shadow.liu@hey900.com>
2017-11-07 17:56:13 +0800

    奖励金额

--- h9-common/src/main/java/com/h9/common/db/repo/LotteryFlowRepository.java
+++ h9-common/src/main/java/com/h9/common/db/repo/LotteryFlowRepository.java
@@ -26,4 +26,8 @@ public interface LotteryFlowRepository extends BaseRepository<LotteryFlow> {
     @Query("select l from LotteryFlow l where l.reward = ?1 and l.userId = ?2 order by l.createTime desc ")
     LotteryFlow findByReward(Reward reward,Long userId);
 
+
+    @Query("select l from LotteryFlow l where l.userId = ?2 order by l.createTime desc ")
+    List<LotteryFlow> findByReward(Long userId);
+
 }
