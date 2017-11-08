bdbed395e2a815b37cc46d504c453de8d6300854
Chaos <liyuan@hey900.com>
2017-11-08 19:00:46 +0800

    添加非空验证

--- h9-common/src/main/java/com/h9/common/modle/dto/LotteryFlowDTO.java
+++ h9-common/src/main/java/com/h9/common/modle/dto/LotteryFlowDTO.java
@@ -2,11 +2,14 @@ package com.h9.common.modle.dto;
 
 import io.swagger.annotations.ApiModelProperty;
 
+import javax.validation.constraints.Size;
+
 /**
  * @author: George
  * @date: 2017/11/8 11:26
  */
 public class LotteryFlowDTO extends PageDTO{
+    @Size(max = 64,message = "兑奖码过长")
     @ApiModelProperty(value = "兑奖码")
     private String code;
 
