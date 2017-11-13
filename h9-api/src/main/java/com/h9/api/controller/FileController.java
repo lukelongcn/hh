780fb6017015ae955a5a953ae9cc9a74d6a48f7f
longdenghui@hey900.com <longdenghui@hey900.com>
2017-11-13 16:08:55 +0800

    commit

--- h9-api/src/main/java/com/h9/api/controller/FileController.java
+++ h9-api/src/main/java/com/h9/api/controller/FileController.java
@@ -30,14 +30,14 @@ public class FileController {
     @Resource
     private FileService fileService;
 
-//    /**
-//     * description: 上传文件
-//     */
-//
-//    @PostMapping("/file/upload")
-//    public Result upload(MultipartFile file) {
-//        return fileService.fileUpload(file);
-//    }
+    /**
+     * description: 上传文件
+     */
+
+    @PostMapping("/file/upload")
+    public Result upload(MultipartFile file) {
+        return fileService.fileUpload(file);
+    }
 
 
 
@@ -45,10 +45,10 @@ public class FileController {
      * description: 上传文件
      */
 
-    @PostMapping("/file/upload")
+    @PostMapping("/file/upload2")
     public Result upload(MultipartFile[] file) {
 
-        if(file.length <=0){
+        if(file == null || file.length <=0){
             return Result.fail("请选择图片");
         }
 
