011878f23582ce011e05eec1f493fc66248aeb84
shadow.liu <shadow.liu@hey900.com>
2017-11-14 14:14:15 +0800

    maven 颁布

--- h9-transfer/src/main/java/com/transfer/db/repo/UserInfoRepository.java
+++ h9-transfer/src/main/java/com/transfer/db/repo/UserInfoRepository.java
@@ -2,6 +2,7 @@ package com.transfer.db.repo;
 
 
 import com.h9.common.base.BaseRepository;
+import com.transfer.db.BasicRepository;
 import com.transfer.db.entity.UserInfo;
 import org.springframework.stereotype.Repository;
 
@@ -12,7 +13,7 @@ import org.springframework.stereotype.Repository;
  * @date: 2016年6月27日 下午3:18:36
  */
 @Repository
-public interface UserInfoRepository extends BaseRepository<UserInfo> {
+public interface UserInfoRepository extends BasicRepository<UserInfo> {
 
 
 }
