b6494540e9587c25eca79ba9e5801eb20d6480bc
Chaos <liyuan@hey900.com>
2017-11-14 17:12:54 +0800

    删除文章开始和结束代码

--- h9-api/src/main/java/com/h9/api/controller/ArticleController.java
+++ h9-api/src/main/java/com/h9/api/controller/ArticleController.java
@@ -1,16 +1,12 @@
 package com.h9.api.controller;
 
-import com.h9.api.interceptor.Secured;
 import com.h9.api.model.vo.ArticleVO;
 import com.h9.api.service.ArticleService;
 import com.h9.common.base.Result;
-import com.h9.common.db.entity.Article;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
-import java.util.Map;
 
 /**
  * Created by 李圆
