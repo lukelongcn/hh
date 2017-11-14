d5f88f5e08d2a0861da23139022a2edbaf752345
Chaos <liyuan@hey900.com>
2017-11-14 17:30:43 +0800

    文章详情

--- h9-api/src/main/java/com/h9/api/service/ArticleService.java
+++ h9-api/src/main/java/com/h9/api/service/ArticleService.java
@@ -31,8 +31,7 @@ public class ArticleService {
     public Result findOne(Long id){
         Article article =  articleReposiroty.findOne(id);
         ArticleVO articleVO = new ArticleVO(articleReposiroty.findOne(id));
-        articleVO.setStartTime(DateUtil.formatDate(article.getStartTime(), DateUtil.FormatType.MINUTE));
-        articleVO.setEndTime(DateUtil.formatDate(article.getEndTime(), DateUtil.FormatType.MINUTE));
+        articleVO.setStartTime(DateUtil.formatDate(article.getCreateTime(), DateUtil.FormatType.MINUTE));
         return Result.success(articleVO);
     }
 
