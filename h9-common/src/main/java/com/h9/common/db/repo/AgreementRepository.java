f31e5e3b184f965c380cb425be2d4b9dad608c8e
Chaos <liyuan@hey900.com>
2017-11-20 14:16:00 +0800

    单页配置

--- h9-common/src/main/java/com/h9/common/db/repo/AgreementRepository.java
+++ h9-common/src/main/java/com/h9/common/db/repo/AgreementRepository.java
@@ -11,22 +11,6 @@ import javax.persistence.LockModeType;
  * Created by liyuan on 2017/11/1.
  */
 public interface AgreementRepository extends BaseRepository<HtmlContent> {
-    /**
-     * 返回页面内容
-     * @param code
-     * @return
-     */
-    @Query("select content from HtmlContent where code=?1 ")
-    String agreement(String code);
-
-
-    /**
-     * 返回页面标题
-     * @param code
-     * @return
-     */
-    @Query("select title from HtmlContent where code=?1 ")
-    String getTitle(String code);
 
     /**
      * 返回单页内容
