3943809a13f62783e3ed0398b6f03bf92c2651aa
Chaos <liyuan@hey900.com>
2017-11-27 16:54:44 +0800

    address类字段修改

--- h9-common/src/main/java/com/h9/common/db/entity/Address.java
+++ h9-common/src/main/java/com/h9/common/db/entity/Address.java
@@ -27,28 +27,28 @@ public class Address extends BaseEntity {
     @Column(name = "user_id", columnDefinition = "bigint(20) default null COMMENT '用户id'")
     private Long userId;
 
-    @Column(name = "name",columnDefinition = "varchar(20) default '' COMMENT '用户名'")
+    @Column(name = "name", nullable = false, columnDefinition = "varchar(20) default '' COMMENT '用户名'")
     private String name;
 
-    @Column(name = "phone",columnDefinition = "varchar(11) default '' COMMENT '手机号'")
+    @Column(name = "phone", nullable = false, columnDefinition = "varchar(11) default '' COMMENT '手机号'")
     private String phone;
     
-    @Column(name = "address", columnDefinition = "varchar(200) default '' COMMENT '地址'")
+    @Column(name = "address", nullable = false, columnDefinition = "varchar(200) default '' COMMENT '地址'")
     private String address;
-    
-    @Column(name = "default_address",columnDefinition = "tinyint default 1 COMMENT '默认地址'")
-    private Integer defaultAddress = 1;
 
-    @Column(name = "province",  columnDefinition = "varchar(50) default '' COMMENT '省'")
+    @Column(name = "default_address",nullable = false,columnDefinition = "tinyint default 1 COMMENT '默认地址'")
+    private Integer defaultAddresss = 1;
+
+    @Column(name = "province", nullable = false, columnDefinition = "varchar(50) default '' COMMENT '省'")
     private String province;
 
-    @Column(name = "city",  columnDefinition = "varchar(50) default '' COMMENT '城市'")
+    @Column(name = "city", nullable = false, columnDefinition = "varchar(50) default '' COMMENT '城市'")
     private String city;
 
-    @Column(name = "distict",  columnDefinition = "varchar(50) default '' COMMENT '区'")
+    @Column(name = "distict", nullable = false, columnDefinition = "varchar(50) default '' COMMENT '区'")
     private String distict;
 
-    @Column(name = "provincial_city", columnDefinition = "varchar(50) default '' COMMENT '城市编号'")
+    @Column(name = "provincial_city", nullable = false, columnDefinition = "varchar(50) default '' COMMENT '城市编号'")
     private String provincialCyty;
 
 
@@ -93,11 +93,11 @@ public class Address extends BaseEntity {
     }
 
     public Integer getDefaultAddresss() {
-        return defaultAddress;
+        return defaultAddresss;
     }
 
     public void setDefaultAddresss(Integer defaultAddresss) {
-        this.defaultAddress = defaultAddresss;
+        this.defaultAddresss = defaultAddresss;
     }
 
     public String getProvince() {
