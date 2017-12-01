a1a18f5df53d87d29479a75a5b00357d4941d695
qiuhonghui <qiuhonghui@hey900.com>
2017-12-01 17:26:53 +0800

    完成白名单

---
+++ h9-admin/src/main/java/com/h9/admin/model/dto/basis/WhiteListEditDTO.java
@@ -0,0 +1,8 @@
+package com.h9.admin.model.dto.basis;
+
+/**
+ * @author: George
+ * @date: 2017/12/1 11:05
+ */
+public class WhiteListAddDTO {
+}
---
+++ h9-common/src/main/java/com/h9/common/modle/vo/admin/basis/WhiteListVO.java
@@ -0,0 +1,8 @@
+package com.h9.common.modle.vo.admin.basis;
+
+/**
+ * @author: George
+ * @date: 2017/12/1 16:37
+ */
+public class WhiteListVO {
+}
