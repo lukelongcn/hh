fb859c84c1951b9daa4733e5b9e8603f34748636
longdenghui@hey900.com <longdenghui@hey900.com>
2017-12-07 18:06:33 +0800

    version update 调整

--- h9-api/src/main/java/com/h9/api/service/HomeService.java
+++ h9-api/src/main/java/com/h9/api/service/HomeService.java
@@ -111,21 +111,30 @@ public class HomeService {
      * @param client
      * @return
      */
-    public Result version(Integer version, Integer client) {
+    public Result version(String version, Integer client) {
 
-        //检查是否有新版本
-        Version lastVersion = versionRepository.findLastVersion(client, version);
-        if (lastVersion.getVersionNumber().equals(version)) return Result.success("已是最新版本");
+//        //检查是否有新版本
+//        Version lastVersion = versionRepository.findLastVersion(client, version);
+//        if (lastVersion.getVersionNumber().equals(version)) return Result.success("已是最新版本");
+//
+//        List<Version> forceUpdateVersion = versionRepository.findForceUpdateVersion(client, version);
+//        if (CollectionUtils.isEmpty(forceUpdateVersion)) {
+//            //当前版本与最新版本之间没有强制升级版,返回最新版
+//            return Result.success(new UpdateInfoVO(lastVersion));
+//        } else {
+//            //有强制升级版，返回最新版，将升级策略改成强升
+//            UpdateInfoVO updateInfoVO = new UpdateInfoVO(lastVersion);
+//            updateInfoVO.setUpdateType(3);
+//            return Result.success(updateInfoVO);
+//        }
 
-        List<Version> forceUpdateVersion = versionRepository.findForceUpdateVersion(client, version);
-        if (CollectionUtils.isEmpty(forceUpdateVersion)) {
-            //当前版本与最新版本之间没有强制升级版,返回最新版
-            return Result.success(new UpdateInfoVO(lastVersion));
+        Version newVersion = versionRepository.findNewVersion(client);
+
+        if (newVersion.getVersion().equals(version)) {
+            return Result.success("已是最新版本");
         } else {
-            //有强制升级版，返回最新版，将升级策略改成强升
-            UpdateInfoVO updateInfoVO = new UpdateInfoVO(lastVersion);
-            updateInfoVO.setUpdateType(3);
-            return Result.success(updateInfoVO);
+            return Result.success(new UpdateInfoVO(newVersion));
         }
+
     }
 }
