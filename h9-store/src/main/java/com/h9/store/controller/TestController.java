5afe1fce88cfef093e72bc47afbf215c6514be57
shadow.liu <shadow.liu@hey900.com>
2017-12-07 14:38:53 +0800

    订单信息

--- h9-store/src/main/java/com/h9/store/controller/TestController.java
+++ h9-store/src/main/java/com/h9/store/controller/TestController.java
@@ -2,28 +2,20 @@ package com.h9.store.controller;
 
 import com.h9.common.base.Result;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by itservice on 2017/11/29.
  */
 @RestController
-public class HelloController {
+@RequestMapping("test")
+public class TestController {
 
 
     @GetMapping("/hello")
     public Result hello(){
-        System.out.println("kdfsfdsfdsfdsddddddddddddd");
-        show();
-        show2();
         return Result.success();
     }
 
-    public void show(){
-        System.out.println("show................");
-    }
-
-    public void show2(){
-        System.out.println("show2222................");
-    }
 }
