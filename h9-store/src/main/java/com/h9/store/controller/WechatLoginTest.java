f6b2755d1792d1744ffaa7109810090fdecff0dd
longdenghui@hey900.com <longdenghui@hey900.com>
2017-12-11 17:17:19 +0800

    commit

--- h9-store/src/main/java/com/h9/store/controller/WechatLoginTest.java
+++ h9-store/src/main/java/com/h9/store/controller/WechatLoginTest.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,8 +23,10 @@ import java.util.Map;
 public class WechatLoginTest {
 
     private Logger logger = Logger.getLogger(this.getClass());
+
     @GetMapping("/test/wx/login")
-    public Result wxLoginTest() {
+    public void wxLoginTest(HttpServletResponse response) throws IOException {
+
         String appId = "wx88536d7560a36bc1";
         String redirectUrl = "http://api-test-h9.thy360.com/h9/store/wxlogin";
         String appsecret = "c7859a462234521b1c9fb6ad66585e36";
@@ -30,14 +35,11 @@ public class WechatLoginTest {
         String url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s" +
                 "&redirect_uri=%s&response_type=code&scope=%s";
         url = String.format(url, appId, redirectUrl, scope);
-        RestTemplate restTemplate = new RestTemplate();
-        Map<String, Object> params = new HashMap<>();
-        String res = restTemplate.getForObject(url, String.class, params);
-        return Result.fail(res);
+        response.sendRedirect(url);
     }
 
     @GetMapping("/wxlogin")
-    public void code(@RequestParam String code,@RequestParam String state){
-        logger.info("code : "+code);
+    public void code(@RequestParam String code, @RequestParam String state) {
+        logger.info("code : " + code);
     }
 }
