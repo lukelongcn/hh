d3e8ef91bc4af3080bc1b2d143d86544ff87d5b2
longdenghui@hey900.com <longdenghui@hey900.com>
2017-12-11 16:59:18 +0800

    commit

--- h9-store/src/main/java/com/h9/store/controller/WechatLoginTest.java
+++ h9-store/src/main/java/com/h9/store/controller/WechatLoginTest.java
@@ -2,23 +2,42 @@ package com.h9.store.controller;
 
 
 import com.h9.common.base.Result;
+import org.jboss.logging.Logger;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by itservice on 2017/12/11.
  */
-@RestController
+
+@Controller
 public class WechatLoginTest {
 
+    private Logger logger = Logger.getLogger(this.getClass());
     @GetMapping("/test/wx/login")
-    public Result wxLoginTest(){
+    public Result wxLoginTest() {
         String appId = "wx88536d7560a36bc1";
-        String redirectUrl = "";
+        String redirectUrl = "http://api-test-h9.thy360.com/h9/store/wxlogin";
         String appsecret = "c7859a462234521b1c9fb6ad66585e36";
+        String scope = "snsapi_userinfo";
 
-        String url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=APPID" +
-                "&redirect_uri=REDIRECT_URI&response_type=code&scope=SCOPE&state=STATE";
-        return null;
+        String url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s" +
+                "&redirect_uri=%s&response_type=%s&scope=%s";
+        url = String.format(url, appId, redirectUrl, appsecret, scope);
+        RestTemplate restTemplate = new RestTemplate();
+        Map<String, Object> params = new HashMap<>();
+        String res = restTemplate.getForObject(url, String.class, params);
+        return Result.fail(res);
+    }
+
+    @GetMapping("/wxlogin")
+    public void code(@RequestParam String code,@RequestParam String state){
+        logger.info("code : "+code);
     }
 }
