45cd2d8a27f4e8e67a467a6946e57f53e8aeec2a
Chaos <liyuan@hey900.com>
2017-12-25 11:41:07 +0800

    版本下载地址接口

--- h9-api/src/main/java/com/h9/api/service/GlobalService.java
+++ h9-api/src/main/java/com/h9/api/service/GlobalService.java
@@ -6,6 +6,8 @@ import com.h9.common.db.repo.GlobalPropertyRepository;
 
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+
 import javax.annotation.Resource;
 import javax.transaction.Transactional;
 
@@ -21,16 +23,19 @@ public class GlobalService {
 
     @Transactional
     public Result version(Integer client) {
+        HashMap map = new HashMap();
         // 安卓
         if (client == 1) {
             GlobalProperty globalProperty = globalPropertyRepository.findByCode("androidDownload");
             System.out.println(globalProperty.getVal());
-            return Result.success(globalProperty.getVal());
+            map.put("redirect",globalProperty.getVal());
+            return Result.success(map);
         }
         // IOS
         if (client == 2) {
             GlobalProperty globalProperty1 = globalPropertyRepository.findByCode("iosDownload");
-            return Result.success(globalProperty1.getVal());
+            map.put("redirect",globalProperty1.getVal());
+            return Result.success(map);
         }
         return Result.fail("请求失败，接口调用出错");
     }
