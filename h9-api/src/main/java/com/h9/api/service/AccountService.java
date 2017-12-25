0367deeeb140ae9878dd4d588b1d114f84c183eb
longdenghui@hey900.com <longdenghui@hey900.com>
2017-12-25 10:56:38 +0800

    查看sql日志

--- h9-api/src/main/java/com/h9/api/service/AccountService.java
+++ h9-api/src/main/java/com/h9/api/service/AccountService.java
@@ -116,9 +116,8 @@ public class AccountService {
 
     public Result couponeList(Long userId, int page, int limit) {
 
-        PageRequest pageRequest = orderItemReposiroty.pageRequest(page, limit);
-        Page<Orders> orders = ordersReposiroty.findDiDiCardByUser(userId, GoodsType.GoodsTypeEnum.DIDI_CARD.getCode(), pageRequest);
-
+        Page<Orders> orders = ordersReposiroty.findDiDiCardByUser(userId, GoodsType.GoodsTypeEnum.DIDI_CARD.getCode(), new PageRequest(page,limit));
+        logger.info("ordersList : "+orders.getContent().size());
         return Result.success(new PageResult<>(orders).result2Result(ord -> {
 
             List<OrderItems> list = ord.getOrderItems();
