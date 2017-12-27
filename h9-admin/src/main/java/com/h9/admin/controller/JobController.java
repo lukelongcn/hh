b946679541cc9c4dcdd8a1bab2f7acfa2e51d948
longdenghui@hey900.com <longdenghui@hey900.com>
2017-12-27 12:51:10 +0800

    commit

--- h9-admin/src/main/java/com/h9/admin/controller/JobController.java
+++ h9-admin/src/main/java/com/h9/admin/controller/JobController.java
@@ -2,6 +2,7 @@ package com.h9.admin.controller;
 
 import com.h9.admin.job.BlackListJob;
 import com.h9.common.base.Result;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +17,15 @@ public class JobController {
     @Resource
     private BlackListJob blackListJob;
 
+    @Value("${h9.current.envir}")
+    private String currentEnvir;
+
     @GetMapping("/job/blackList")
     public Result blackListJob() {
+//        if (currentEnvir.equals("product")) {
+//            return Result.fail("不支持此环境");
+//        }
+
         blackListJob.scan();
         return Result.success("开始执行定时任务");
     }
