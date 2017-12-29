2a3cd24c7d78be685e5e84c0d245623dfa497b83
Chaos <liyuan@hey900.com>
2017-12-29 18:23:54 +0800

    签到

---
+++ h9-api/src/main/java/com/h9/api/controller/SignController.java
@@ -0,0 +1,12 @@
+package com.h9.api.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by 李圆 on 2017/12/29
+ */
+@RestController
+public class SignController {
+
+
+}
