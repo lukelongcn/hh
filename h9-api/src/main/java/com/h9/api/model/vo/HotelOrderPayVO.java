a7a974317c1cf5e9887216e79b404ed1a0ea6e1d
longdenghui@hey900.com <longdenghui@hey900.com>
2018-01-05 10:39:14 +0800

    获取支付信息

--- h9-api/src/main/java/com/h9/api/model/vo/HotelOrderPayVO.java
+++ h9-api/src/main/java/com/h9/api/model/vo/HotelOrderPayVO.java
@@ -1,7 +1,12 @@
 package com.h9.api.model.vo;
 
+import com.h9.common.db.entity.hotel.HotelOrder;
+import com.h9.common.db.entity.user.UserAccount;
+import com.h9.common.utils.MoneyUtils;
 import lombok.Data;
 import lombok.experimental.Accessors;
+
+import java.math.BigDecimal;
 
 /**
  * Created by itservice on 2018/1/3.
@@ -17,4 +22,13 @@ public class HotelOrderPayVO {
     private String balance;
 
     private Long hotelOrderId;
+
+    public HotelOrderPayVO(){}
+
+    public HotelOrderPayVO(HotelOrder hotelOrder, UserAccount userAccount, BigDecimal totalMoney){
+        this.setBalance(MoneyUtils.formatMoney(userAccount.getBalance()))
+                .setOrderMoney(MoneyUtils.formatMoney(totalMoney))
+                .setTips("请在23:59之前完成支付，过期将自动取消订单")
+                .setHotelOrderId(hotelOrder.getId());
+    }
 }
