1ec8338b5b0943ba9e2913fb4c894d8b04ae1299
Chaos <liyuan@hey900.com>
2018-01-08 17:16:37 +0800

    签到

--- h9-api/src/main/java/com/h9/api/service/SignService.java
+++ h9-api/src/main/java/com/h9/api/service/SignService.java
@@ -157,11 +157,18 @@ public class SignService {
         //获取用户上次签到时间
         Date checkDate = userSign.getCreateTime();
         Date today = DateUtil.getTimesMorning();
-        // 如果用户不是第一次进入页面且今日没有签到
+        // 如果用户今日没有签到  并且断签或不是第一次进入页面
         UserSignMessageVO userSignMessageVO;
        if (checkDate.before(today)){
-             userSignMessageVO = new UserSignMessageVO(userAccount.getBalance(),user,userSign,
-                    listSignVO,0);
+             if (checkDate.before(DateUtil.getYesterdaymorning())){
+                 user.setSignDays(0);
+                 user = userRepository.saveAndFlush(user);
+                 userSignMessageVO = new UserSignMessageVO(userAccount.getBalance(),user,userSign,
+                         listSignVO,0);
+             }else{
+                 userSignMessageVO = new UserSignMessageVO(userAccount.getBalance(),user,userSign,
+                         listSignVO,0);
+             }
         }else{
            // 如果用户不是第一次进入页面且已签到
            userSignMessageVO = new UserSignMessageVO(userAccount.getBalance(),user,userSign,
