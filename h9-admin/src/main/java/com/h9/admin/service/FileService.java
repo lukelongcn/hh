63c491528b7691b8bd70c73ce2cfc38984e7af8a
qiuhonghui <qiuhonghui@hey900.com>
2018-01-12 15:18:39 +0800

    Revert "修改文件上传"
    This reverts commit 5cf3f7f9c2404a039a77e69d6ab5192e0a4fab9d.

--- h9-admin/src/main/java/com/h9/admin/service/FileService.java
+++ h9-admin/src/main/java/com/h9/admin/service/FileService.java
@@ -43,13 +43,13 @@ public class FileService {
 
     public Result uploadImage(MultipartFile file, String path) {
         if (file == null) return Result.fail("请选择图片");
-        path = this.buildFilePath(path,getExtendName(file.getOriginalFilename()));
+        path = this.buildFilePath("images",path);
         return this.upload(file, path);
     }
 
     public Result uploadFile(MultipartFile file, String path) {
         if (file == null) return Result.fail("请选择文件");
-        path = this.buildFilePath(path,getExtendName(file.getOriginalFilename()));
+        path = this.buildFilePath("file",path);
         return this.upload(file, path);
     }
 
@@ -83,17 +83,16 @@ public class FileService {
     }
 
 
-    private String buildFilePath(String path, String fileType) {
-        StringBuilder key = new StringBuilder(envir);
-        String name = UUID.randomUUID() + "." + fileType;
+    private String buildFilePath(String type,String path) {
+        StringBuilder key = new StringBuilder(type).append("/").append(envir);
         if(StringUtils.isNotBlank(path)){
             if ("/".equals(path)) {
-                key.append(path).append(name);
+                key.append(path).append(UUID.randomUUID());
             }else {
-                key.append(path).append("/").append(name);
+                key.append(path).append("/").append(UUID.randomUUID());
             }
         }else{
-            key.append("/other/").append(name);
+            key.append("/other/").append(UUID.randomUUID());
         }
         return key.toString();
     }
@@ -128,16 +127,6 @@ public class FileService {
         }
 
         return Result.fail("上传失败");
-    }
-
-    public String getExtendName(String originalFilename) {
-        if ((originalFilename != null) && (originalFilename.length() > 0)) {
-            int dot = originalFilename.lastIndexOf('.');
-            if ((dot >-1) && (dot < (originalFilename.length() - 1))) {
-                return originalFilename.substring(dot + 1);
-            }
-        }
-        return originalFilename;
     }
 
   /*  public String uploadCKEditorImage(MultipartFile file, int CKEditorFuncNum) {
