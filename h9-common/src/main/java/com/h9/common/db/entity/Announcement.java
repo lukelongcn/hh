97587a42ccbe384afadb564dd929762fe41eacb7
qiuhonghui <qiuhonghui@hey900.com>
2018-01-16 15:41:48 +0800

    修改描述为text类型

--- h9-common/src/main/java/com/h9/common/db/entity/Announcement.java
+++ h9-common/src/main/java/com/h9/common/db/entity/Announcement.java
@@ -25,7 +25,7 @@ public class Announcement extends BaseEntity {
     @Column(name = "title", nullable = false, columnDefinition = "varchar(128) default '' COMMENT '标题'")
     private String title;
 
-    @Column(name = "content", nullable = false, columnDefinition = "varchar(512) default '' COMMENT '内容'")
+    @Column(name = "content", nullable = false, columnDefinition = "text default '' COMMENT '内容'")
     private String content;
 
     @Column(name = "url",  columnDefinition = "varchar(256) default '' COMMENT '跳转链接'")
