a32611784b6a59175663b8ba4cdfe2507fd1624f
Chaos <liyuan@hey900.com>
2018-01-23 11:24:49 +0800

    通过状态

--- h9-admin/src/main/java/com/h9/admin/model/vo/StickDetailVO.java
+++ h9-admin/src/main/java/com/h9/admin/model/vo/StickDetailVO.java
@@ -22,26 +22,29 @@ public class StickDetailVO {
     private String content = "";
     private Long userId;
     private String nickName;
-    private Integer lockState = 1;
-    private Integer operationState = 1;
+    private Integer lockState;
+    private Integer operationState;
     private String ip;
-    private Integer state = 1;
-    private Integer rewardCount = 0;
+    private Integer state;
+    private Integer rewardCount;
     private BigDecimal rewardMoney = new BigDecimal(0);
     private String createTime;
     private String url;
 
-    public StickDetailVO(Stick stick){
-        id = stick.getId();
-        typeId = stick.getStickType().getId();
-        stickTypeName = stick.getStickType().getName();
-        title = stick.getTitle();
-        content = stick.getContent();
-        userId = stick.getUser().getId();
-        nickName = stick.getUser().getNickName();
-        lockState = stick.getLockState();
-        rewardCount = stick.getRewardCount();
-        createTime = DateUtil.getSpaceTime(stick.getCreateTime(),new Date());
+    public StickDetailVO(Stick stick) {
+        this.id = stick.getId();
+        this.typeId = stick.getStickType().getId();
+        this.stickTypeName = stick.getStickType().getName();
+        this.title = stick.getTitle();
+        this.content = stick.getContent();
+        this.userId = stick.getUser().getId();
+        this.nickName = stick.getUser().getNickName();
+        this.lockState = stick.getLockState();
+        this.operationState = stick.getOperationState();
+        this.ip = stick.getIp();
+        this.state = stick.getState();
+        this.rewardCount = stick.getRewardCount();
+        this.createTime = DateUtil.getSpaceTime(stick.getCreateTime(), new Date());
     }
     public StickDetailVO(){
 
