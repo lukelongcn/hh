de5762bd3451ee5e3002ab204dc6c914d7d91425
shadow.liu <shadow.liu@hey900.com>
2018-01-24 15:24:24 +0800

    评论

--- h9-common/src/main/java/com/h9/common/db/repo/StickCommentRepository.java
+++ h9-common/src/main/java/com/h9/common/db/repo/StickCommentRepository.java
@@ -39,12 +39,12 @@ public interface StickCommentRepository extends BaseRepository<StickComment> {
     List<StickComment> findByBackId(long stickCommentParentId);
 
 
-    @Query("select s from StickComment s where s.answerUser.id = ?1  and s.state = 1  and " +
+    @Query("select s from StickComment s where s.stick.id = ?1  and s.state = 1  and " +
             "s.operationState = 1 order by s.createTime DESC")
     Page<StickComment> findCommentList(long userId, Pageable pageRequest);
 
-    default PageResult<StickComment> findCommentList(long userId, Integer page, Integer limit){
-        Page<StickComment> stickComment =  findCommentList(userId, pageRequest(page,limit));
+    default PageResult<StickComment> findCommentList(long stickId, Integer page, Integer limit){
+        Page<StickComment> stickComment =  findCommentList(stickId, pageRequest(page,limit));
         return new PageResult(stickComment);
     }
 
