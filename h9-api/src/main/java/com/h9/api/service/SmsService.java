08cc7e894fbb8cb0d68217817dd1c430ed7f8df7
shadow.liu <shadow.liu@hey900.com>
2018-01-26 16:19:17 +0800

    短信验证码

--- h9-api/src/main/java/com/h9/api/service/SmsService.java
+++ h9-api/src/main/java/com/h9/api/service/SmsService.java
@@ -168,12 +168,6 @@ public class SmsService {
     private Result sendSMSValdite(Long userId, String phone, int smsType) {
 
         if(smsType == SMSTypeEnum.CASH_RECHARGE.getCode()){
-
-            return Result.fail("提现功能正在维护中");
-        }
-
-
-        if(smsType == SMSTypeEnum.CASH_RECHARGE.getCode()){
             //判断提现额度
             BigDecimal todayCanWithdrawMoney = consumeService.getUserWithdrawTodayMoney(userId);
 
