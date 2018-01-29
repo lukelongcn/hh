1bf10503bbc146b4c0c739f3196ca41ad9c5a164
Chaos <liyuan@hey900.com>
2018-01-29 16:30:04 +0800

    后台 房间必填选项

--- h9-admin/src/main/java/com/h9/admin/model/dto/hotel/EditRoomDTO.java
+++ h9-admin/src/main/java/com/h9/admin/model/dto/hotel/EditRoomDTO.java
@@ -27,7 +27,7 @@ public class EditRoomDTO {
     private String roomName;
 
     @ApiModelProperty("房间类型名")
-    @NotNull(message = "")
+    @NotNull(message = "请填写房间类型名")
     private String typeName;
 
     @ApiModelProperty("房间尺寸")
