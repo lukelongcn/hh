fef90a68e3c5385b77c7f78c3393877146b4ded3
Chaos <liyuan@hey900.com>
2018-01-31 14:28:40 +0800

    api 贴子

--- h9-api/src/main/java/com/h9/api/service/StickService.java
+++ h9-api/src/main/java/com/h9/api/service/StickService.java
@@ -154,7 +154,7 @@ public class StickService {
     public Result addStick(Long userId, StickDto stickDto, HttpServletRequest request){
         StickType stickType = stickTypeRepository.findOne(stickDto.getTypeId());
         if(stickType == null){
-           //return Result.fail("请选择分类");
+           return Result.fail("请选择分类");
         }
         if (stickType != null){
             // 只有管理员权限能发帖
