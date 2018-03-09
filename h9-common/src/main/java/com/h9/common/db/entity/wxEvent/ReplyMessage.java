c5eeb948437bd543d287bb76b668c62f27be6366
Chaos <liyuan@hey900.com>
2018-03-09 16:27:56 +0800

    自动回复 数据库表

--- h9-common/src/main/java/com/h9/common/db/entity/wxEvent/ReplyMessage.java
+++ h9-common/src/main/java/com/h9/common/db/entity/wxEvent/ReplyMessage.java
@@ -1,9 +1,11 @@
 package com.h9.common.db.entity.wxEvent;
 
 import com.h9.common.base.BaseEntity;
+import com.h9.common.db.entity.community.StickType;
 
 import javax.persistence.*;
 
+import static java.util.Arrays.stream;
 import static javax.persistence.GenerationType.IDENTITY;
 
 /**
@@ -15,6 +17,9 @@ public class ReplyMessage extends BaseEntity{
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
+
+    @Column(name = "order_type",columnDefinition = "varchar(200) COMMENT '规则名'")
+    private String orderType;
 
     @Column(name = "event_type",columnDefinition = "varchar(200) COMMENT '事件类型'")
     private String eventType;
@@ -42,7 +47,9 @@ public class ReplyMessage extends BaseEntity{
     public enum matchStrategyEnum{
         ALL_MATCH(1,"完全匹配"),
         SECTION_MATCH(2,"半匹配"),
-        REGEX_MATCH(3, "正则匹配");
+        REGEX_MATCH(3, "正则匹配"),
+        AUTOMATIC_REPLY(4,"自动回复"),
+        FOLLOW_REPLY(5,"关注回复");
         private int code;
         private String desc;
 
@@ -50,11 +57,49 @@ public class ReplyMessage extends BaseEntity{
             this.code = code;
             this.desc = desc;
         }
+        public static String getDescByCode(int code){
+            ReplyMessage.matchStrategyEnum matchStrategyEnum = stream(values()).filter(o -> o.getCode()==code).limit(1).findAny().orElse(null);
+            return matchStrategyEnum==null?null:matchStrategyEnum.getDesc();
+        }
 
+        public int getCode() {
+            return code;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+    }
+
+    public enum  orderTypeEnum{
+        LUCKY_DRAW(1,"抽奖活动"),
+        GOODS(2,"商品"),
+        KEYWORD_MATCHING(3,"关键字匹配"),
+        AUTOMATIC_REPLY(4,"自动回复"),
+        FOLLOW_REPLY(5,"关注回复"),
+        REGULAR_MATCHING(6,"正则匹配");
+
+        private int code;
+        private String desc;
+        orderTypeEnum(int code, String desc) {
+            this.code = code;
+            this.desc = desc;
+        }
+        public static String getDescByCode(int code){
+            ReplyMessage.orderTypeEnum orderTypeEnum = stream(values()).filter(o -> o.getCode()==code).limit(1).findAny().orElse(null);
+            return orderTypeEnum==null?null:orderTypeEnum.getDesc();
+        }
+        public int getCode() {
+            return code;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
     }
 
     public static void main(String[] args) {
-        String s = matchStrategyEnum.ALL_MATCH.toString();
+        String s = matchStrategyEnum.getDescByCode(matchStrategyEnum.ALL_MATCH.getCode());
         System.out.println(s);
     }
 }
