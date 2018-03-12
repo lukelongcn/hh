e916caefbc3c218e5f4a3d0ba91c1befc69ea227
Chaos <liyuan@hey900.com>
2018-03-12 16:29:37 +0800

    CADTA

--- h9-api/src/main/java/com/h9/api/service/wxEvent/model/Message4wx.java
+++ h9-api/src/main/java/com/h9/api/service/wxEvent/model/Message4wx.java
@@ -1,15 +1,19 @@
 package com.h9.api.service.wxEvent.model;
 
 import com.h9.api.provider.handler.CDataContentHandler;
+import com.h9.common.utils.XMLUtils;
 import lombok.Data;
+import lombok.Setter;
 import lombok.experimental.Accessors;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * 文本消息
  */
-@Data
+@Setter
 @Accessors(chain = true)
 @XmlRootElement(name = "xml")
 public class Message4wx {
@@ -29,8 +33,51 @@ public class Message4wx {
 
     public static void main(String[] args) throws Exception{
         Message4wx message4Wx = new Message4wx();
-        message4Wx.setToUserName("<kk").setFromUserName("<ll").setCreateTime(2017);
-        String s = CDataContentHandler.ojbectToXmlWithCDATA(Message4wx.class, message4Wx);
+        message4Wx.setToUserName("kk");
+        message4Wx.setFromUserName("ll");
+        message4Wx.setCreateTime(2017);
+        message4Wx.setMediaId(121323L);
+        String s = CDataContentHandler.ojbectToXmlWithCDATA(Message4wx.class,message4Wx);
         System.out.println(s);
+    }
+
+    @XmlElement(name = "ToUserName")
+    public String getToUserName() {
+        return ToUserName;
+    }
+
+    @XmlElement(name = "FromUserName")
+    public String getFromUserName() {
+        return FromUserName;
+    }
+
+    @XmlElement(name = "CreateTime")
+    public int getCreateTime() {
+        return CreateTime;
+    }
+
+    @XmlElement(name = "MsgType")
+    public String getMsgType() {
+        return MsgType;
+    }
+
+    @XmlElement(name = "Content")
+    public String getContent() {
+        return Content;
+    }
+
+    @XmlElement(name = "Title")
+    public String getTitle() {
+        return Title;
+    }
+
+    @XmlElement(name = "Description")
+    public String getDescription() {
+        return Description;
+    }
+
+    @XmlElement(name = "MediaId")
+    public Long getMediaId() {
+        return MediaId;
     }
 }
