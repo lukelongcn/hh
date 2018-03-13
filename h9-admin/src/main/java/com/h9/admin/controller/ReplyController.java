e42093a8ab7ba43da32d6630b2bebff5cdc89eb5
Chaos <liyuan@hey900.com>
2018-03-13 16:18:56 +0800

    后台编辑

--- h9-admin/src/main/java/com/h9/admin/controller/ReplyController.java
+++ h9-admin/src/main/java/com/h9/admin/controller/ReplyController.java
@@ -4,6 +4,8 @@ package com.h9.admin.controller;
 import com.h9.admin.model.dto.ReplyDTO;
 import com.h9.admin.service.ReplyService;
 import com.h9.common.base.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -14,6 +16,7 @@ import javax.validation.Valid;
  * Created by 李圆 on 2018/1/15
  */
 @RestController
+@Api("微信回复规则操作")
 @RequestMapping("/wx/reply")
 public class ReplyController {
 
@@ -23,6 +26,7 @@ public class ReplyController {
     /**
      * 添加微信回复规则
      */
+    @ApiOperation("添加微信回复规则")
     @PostMapping("/add")
     public Result addRule(@Valid@RequestBody ReplyDTO replyDTO){
         return replyService.addRule(replyDTO);
@@ -32,6 +36,7 @@ public class ReplyController {
     /**
      * 禁用
      */
+    @ApiOperation("禁用微信回复规则")
     @PostMapping("/disable/{id}")
     public Result disable(@PathVariable long id){
         return replyService.disable(id);
@@ -40,6 +45,7 @@ public class ReplyController {
     /**
      * 获取单个详情
      */
+    @ApiOperation("获取单个微信回复规则详情")
     @GetMapping("/detail/{id}")
     public Result detail(@PathVariable long id){
         return replyService.detail(id);
@@ -48,6 +54,7 @@ public class ReplyController {
     /**
      * 获取规则列表
      */
+    @ApiOperation("获取微信回复规则列表")
     @GetMapping("/all")
     public Result all(){
         return replyService.all();
@@ -56,6 +63,7 @@ public class ReplyController {
     /**
      * 编辑
      */
+    @ApiOperation("编辑微信回复规则")
     @PostMapping("update/{id}")
     public Result update(@PathVariable long id,@Valid@RequestBody ReplyDTO replyDTO){
         return replyService.update(id,replyDTO);
@@ -64,6 +72,7 @@ public class ReplyController {
     /**
      * 微信回复规则类型
      */
+    @ApiOperation("微信回复规则类型")
     @GetMapping("/ruleType")
     public Result ruleType(){
         return replyService.getRuleType();
@@ -72,6 +81,7 @@ public class ReplyController {
     /**
      * 微信回复类型
      */
+    @ApiOperation("微信回复类型")
     @GetMapping("/replyType")
     public Result getReplyType(){
         return replyService.getReplyType();
--- h9-admin/src/main/java/com/h9/admin/model/vo/ReplyMessageVO.java
+++ h9-admin/src/main/java/com/h9/admin/model/vo/ReplyMessageVO.java
@@ -1,6 +1,7 @@
 package com.h9.admin.model.vo;
 
 import com.h9.common.db.entity.wxEvent.ReplyMessage;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.springframework.beans.BeanUtils;
 
@@ -14,24 +15,25 @@ import org.springframework.beans.BeanUtils;
  */
 @Data
 public class ReplyMessageVO {
+    @ApiModelProperty(value = "id" )
     private Long id;
-
+    @ApiModelProperty(value = "规则名" )
     private String orderName;
-
+    @ApiModelProperty(value = "微信事件类型" )
     private String eventType;
-
+    @ApiModelProperty(value = "回复内容类型" )
     private String contentType;
-
+    @ApiModelProperty(value = "回复内容" )
     private String content;
-
+    @ApiModelProperty(value = "匹配正则" )
     private String matchRegex;
-
+    @ApiModelProperty(value = "使用状态 1 使用 2 禁用" )
     private Integer status;
-
+    @ApiModelProperty(value = "排序" )
     private Integer sort;
-
+    @ApiModelProperty(value = "规则类型" )
     private String matchStrategy;
-
+    @ApiModelProperty(value = "关键字" )
     private String keyWord;
 
     public ReplyMessageVO(ReplyMessage replyMessage){
