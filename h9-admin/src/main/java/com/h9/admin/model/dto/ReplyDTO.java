a7683a4407df035663d2bbbc7c768dece05ca94a
Chaos <liyuan@hey900.com>
2018-03-13 16:22:29 +0800

    后台编辑

--- h9-admin/src/main/java/com/h9/admin/model/dto/ReplyDTO.java
+++ h9-admin/src/main/java/com/h9/admin/model/dto/ReplyDTO.java
@@ -1,5 +1,7 @@
 package com.h9.admin.model.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.hibernate.validator.constraints.NotBlank;
 
@@ -9,35 +11,44 @@ import javax.validation.constraints.Size;
 /**
  * Created by 李圆 on 2018/1/15
  */
+@ApiModel("回复规则信息")
 @Data
 public class ReplyDTO {
 
+    @ApiModelProperty(value = "规则名")
     @Size(min = 2 ,max = 120)
     @NotBlank(message = "规则名必须在2到120个字符之间")
     private String orderName;
     /**
      * 匹配策略  规则类型
      */
+    @ApiModelProperty(value = "规则类型")
     @NotBlank(message = "规则类型必填")
     private String matchStrategy;
 
+    @ApiModelProperty(value = "关键词")
     @Size(max = 120)
     @NotBlank(message = "关键词必填")
     private String keyWord;
 
     // 回复类型
+    @ApiModelProperty(value = "回复类型")
     @NotBlank(message = "回复类型必填")
     private String contentType;
 
+    @ApiModelProperty(value = "回复内容")
     @NotBlank(message = "回复内容必填")
     private String content;
 
     // 正则表达式
+    @ApiModelProperty(value = "正则表达式")
     private String matchRegex;
 
+    @ApiModelProperty(value = "排序")
     @NotNull(message = "排序必填,只能是整数")
     private Integer sort;
 
+    @NotNull(message = "状态 1使用 2 禁用")
     private Integer status;
 
 }
