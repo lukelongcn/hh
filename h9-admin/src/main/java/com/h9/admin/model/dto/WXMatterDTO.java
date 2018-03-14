36acc490eb26c17b554412030afe127e1197caa2
Chaos <liyuan@hey900.com>
2018-03-14 18:01:37 +0800

    微信素材列表

--- h9-admin/src/main/java/com/h9/admin/model/dto/WXMatterDTO.java
+++ h9-admin/src/main/java/com/h9/admin/model/dto/WXMatterDTO.java
@@ -18,6 +18,7 @@ import org.omg.CORBA.PRIVATE_MEMBER;
 @ApiModel("微信素材信息")
 public class WXMatterDTO {
 
+
     @ApiModelProperty("素材的类型，图片（image）、视频（video）、语音 （voice）、图文（news）")
     private String type;
     @ApiModelProperty("从全部素材的该偏移位置开始返回，0表示从第一个素材 返回")
