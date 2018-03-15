49ace30a121e7c38475588059186e18f60fb7066
Chaos <liyuan@hey900.com>
2018-03-15 16:26:01 +0800

    微信搜索條件

--- h9-admin/src/main/java/com/h9/admin/model/vo/ReplyMessageVO.java
+++ h9-admin/src/main/java/com/h9/admin/model/vo/ReplyMessageVO.java
@@ -19,14 +19,10 @@ public class ReplyMessageVO {
     private Long id;
     @ApiModelProperty(value = "规则名" )
     private String orderName;
-    @ApiModelProperty(value = "微信事件类型" )
-    private String eventType;
     @ApiModelProperty(value = "回复内容类型" )
     private String contentType;
     @ApiModelProperty(value = "回复内容" )
     private String content;
-    @ApiModelProperty(value = "匹配正则" )
-    private String matchRegex;
     @ApiModelProperty(value = "使用状态 1 使用 2 禁用" )
     private Integer status;
     @ApiModelProperty(value = "排序" )
