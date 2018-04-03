e1424253276f928ea94dd8f0b2752a99a8e92d81
Chaos <liyuan@hey900.com>
2018-04-03 16:58:38 +0800

    优惠券表

---
+++ h9-common/src/main/java/com/h9/common/db/entity/order/Coupon.java
@@ -0,0 +1,93 @@
+package com.h9.common.db.entity.order;
+
+import com.h9.common.base.BaseEntity;
+import com.h9.common.db.entity.lottery.OrdersLotteryActivity;
+import lombok.Data;
+
+import javax.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+/**
+ * <p>Title:h9-parent</p>
+ * <p>Desription:优惠券表</p>
+ *
+ * @author LiYuan
+ * @Date 2018/4/3
+ */
+@Data
+@Entity
+@Table(name = "coupon")
+public class Coupon extends BaseEntity {
+
+    @Id
+    @SequenceGenerator(name = "h9-apiSeq", sequenceName = "h9-api_SEQ", allocationSize = 1, initialValue = 1)
+    @GeneratedValue(strategy = IDENTITY, generator = "h9-apiSeq")
+    private Long id;
+
+    /**
+     * 状态 1 未生效 0生效中 2已失效
+     * @see OrdersLotteryActivity.statusEnum
+     */
+    @Column(name = "status", nullable = false, columnDefinition = "int  COMMENT '状态 1 未生效 0生效中 2已失效'")
+    private int status;
+
+    @Column(name = "start_time", nullable = false, columnDefinition = "datetime COMMENT '开始时间'")
+    private Date startTime;
+
+    @Column(name = "end_time", nullable = false, columnDefinition = "datetime  COMMENT '结束时间'")
+    private Date endTime;
+
+    @Column(name = "value", columnDefinition = "DECIMAL(10,2) default 0.00 COMMENT '面值'")
+    private BigDecimal value = new BigDecimal(0);
+
+    @Column(name = "condition", columnDefinition = "DECIMAL(10,2) default 0.00 COMMENT '使用条件'")
+    private BigDecimal condition = new BigDecimal(0);
+
+    @Column(name = "left_count", columnDefinition = "int default 0 COMMENT '剩余张数'")
+    private int leftCount;
+
+    public enum statusEnum{
+
+        ENABLE(1,"未生效"),
+        BAN(0,"0生效中"),
+        FINISH(2,"2已失效");
+
+        private int code;
+        private String desc;
+
+        statusEnum(int code, String desc) {
+            this.code = code;
+            this.desc = desc;
+        }
+
+        public int getCode() {
+            return code;
+        }
+
+        public void setCode(int code) {
+            this.code = code;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+
+        public void setDesc(String desc) {
+            this.desc = desc;
+        }
+
+        public static Coupon.statusEnum findByCode(int code){
+            Coupon.statusEnum[] values = values();
+            for(Coupon.statusEnum enumEl : values){
+                if(enumEl.code == code){
+                    return enumEl;
+                }
+            }
+            return null;
+        }
+    }
+}
