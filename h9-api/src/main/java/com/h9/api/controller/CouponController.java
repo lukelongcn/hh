3c3081d9089f5cc964c5dc861ad730e2074678c7
Chaos <liyuan@hey900.com>
2018-04-03 17:14:26 +0800

    api 优惠券接口

---
+++ h9-api/src/main/java/com/h9/api/controller/CouponController.java
@@ -0,0 +1,20 @@
+package com.h9.api.controller;
+
+import com.h9.api.service.CouponService;
+
+import javax.annotation.Resource;
+
+/**
+ * <p>Title:h9-parent</p>
+ * <p>Desription:优惠券接口</p>
+ *
+ * @author LiYuan
+ * @Date 2018/4/3
+ */
+public class CouponController {
+
+    @Resource
+    private CouponService couponService;
+
+
+}
