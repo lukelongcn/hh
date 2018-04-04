9475ce181ba675fdfefda3a0e70baf08cbb8b28f
Chaos <liyuan@hey900.com>
2018-04-04 17:50:31 +0800

    优惠券订单显示

--- h9-common/src/main/java/com/h9/common/db/repo/UserCouponsRepository.java
+++ h9-common/src/main/java/com/h9/common/db/repo/UserCouponsRepository.java
@@ -28,6 +28,6 @@ public interface UserCouponsRepository extends BaseRepository<UserCoupon> {
         return new PageResult(list);
     }
 
-    @Query("select u From UserCoupon u where u.userId =?1 and s.state=1 order by u.id")
-    List<UserCoupon> findByUserId(Long userId);
+    @Query("select u From UserCoupon u where u.userId =?1 and u.state=1 and u.couponId.goodsId.id = ?2 order by u.createTime asc ")
+    List<UserCoupon> findByUserId(Long userId,Long goodId);
 }
