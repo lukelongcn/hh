f2ccb6aa4464d8d2e9bed9d14a38821493464521
Chaos <liyuan@hey900.com>
2018-04-08 17:55:46 +0800

    后天新增优惠券

--- h9-admin/src/main/java/com/h9/admin/controller/CouponController.java
+++ h9-admin/src/main/java/com/h9/admin/controller/CouponController.java
@@ -1,10 +1,9 @@
 package com.h9.admin.controller;
 
+import com.h9.admin.model.dto.CouponsDTO;
 import com.h9.admin.service.CouponService;
 import com.h9.common.base.Result;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -20,9 +19,25 @@ public class CouponController {
     @Resource
     private CouponService couponService;
 
+    /**
+     * 优惠券列表
+     * @param page 页码
+     * @param limit 个数
+     * @return pageRequest
+     */
     @GetMapping("/coupons")
     public Result coupons(@RequestParam(defaultValue = "1") Integer page,
                           @RequestParam(defaultValue = "10") Integer limit){
         return couponService.coupons(page,limit);
+    }
+
+    /**
+     * 新增优惠券
+     * @param couponsDTO 请求对象
+     * @return result
+     */
+    @PostMapping("/addCoupons")
+    public Result addCoupons(@RequestBody CouponsDTO couponsDTO){
+        return couponService.addCoupons(couponsDTO);
     }
 }
--- h9-admin/src/main/java/com/h9/admin/service/CouponService.java
+++ h9-admin/src/main/java/com/h9/admin/service/CouponService.java
@@ -1,13 +1,19 @@
 package com.h9.admin.service;
 
+import com.h9.admin.model.dto.CouponsDTO;
 import com.h9.admin.model.vo.CouponVO;
 import com.h9.common.base.PageResult;
 import com.h9.common.base.Result;
 import com.h9.common.db.entity.order.Coupon;
+import com.h9.common.db.entity.order.Goods;
 import com.h9.common.db.repo.CouponRespository;
+import com.h9.common.db.repo.GoodsReposiroty;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Date;
+import java.util.List;
 
 /**
  * <p>Title:h9-parent</p>
@@ -20,7 +26,8 @@ import javax.annotation.Resource;
 public class CouponService {
     @Resource
     private CouponRespository couponRespository;
-
+    @Resource
+    private GoodsReposiroty goodsReposiroty;
 
     public Result coupons(Integer page, Integer limit) {
         PageResult<Coupon> pageResult = couponRespository.findAll(page, limit);
@@ -28,5 +35,35 @@ public class CouponService {
             return Result.fail("暂无记录");
         }
         return Result.success(pageResult.result2Result(CouponVO::new));
+    }
+
+    public Result addCoupons(CouponsDTO couponsDTO) {
+        List<Long> goodsIdList = couponsDTO.getGoodIdList();
+        if (CollectionUtils.isEmpty(goodsIdList)){
+            return Result.fail("新增优惠券失败");
+        }
+        goodsIdList.forEach(gid->{
+            Coupon coupon = new Coupon();
+            coupon.setTitle(couponsDTO.getTitle());
+            coupon.setCouponType(couponsDTO.getCouponType());
+            coupon.setStartTime(couponsDTO.getStartTime());
+            coupon.setEndTime(couponsDTO.getEndTime());
+            Goods goods = goodsReposiroty.findOne(gid);
+            if (goods != null){
+                coupon.setGoodsId(goods);
+            }
+            // 制券数
+            coupon.setLeftCount(couponsDTO.getAskCount());
+            // 状态
+            if (couponsDTO.getStartTime().after(new Date())){
+                coupon.setStatus(1);
+            } else if (couponsDTO.getEndTime().after(new Date())){
+                coupon.setStatus(2);
+            } else {
+                coupon.setStatus(3);
+            }
+            couponRespository.saveAndFlush(coupon);
+        });
+        return Result.success("新增优惠券成功");
     }
 }
